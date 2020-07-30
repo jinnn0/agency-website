0b1c78d43d3b6b88721a0ed9ef61a20d88a4a0da
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-07-30 17:00:20 +0100

    Updates

---
+++ precache-manifest.834d65d0c7badda02b578bc3ee9d8d47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3b30140479854554910768ac7ad880",
+    "url": "/agency-website/index.html"
+  },
+  {
+    "revision": "7f3c6b339359ce388615",
+    "url": "/agency-website/static/css/main.4a65010b.chunk.css"
+  },
+  {
+    "revision": "4182d9fd2b1136f56da1",
+    "url": "/agency-website/static/js/2.bf53bdb0.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/agency-website/static/js/2.bf53bdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3c6b339359ce388615",
+    "url": "/agency-website/static/js/main.def1ff1d.chunk.js"
+  },
+  {
+    "revision": "98f60a68bc366032bed6",
+    "url": "/agency-website/static/js/runtime-main.31e15fbc.js"
+  },
+  {
+    "revision": "6c13dbdb3a094b7e030fa7f71c7542bd",
+    "url": "/agency-website/static/media/airpod.6c13dbdb.png"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/agency-website/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/agency-website/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "b4b9554b41d3b45333a29dee526e40ec",
+    "url": "/agency-website/static/media/curology.b4b9554b.png"
+  },
+  {
+    "revision": "83cb718025f0d91985be6733d1762e5f",
+    "url": "/agency-website/static/media/yourspace.83cb7180.png"
+  }
+]);
